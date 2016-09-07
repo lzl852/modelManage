56db81e7b564a6dbd35973ca492c51d2664476d6
程泽赫 <varrantczh@sina.com>
2016-09-07 16:11:37 +0800

    module develop

--- webroot-dev/coolie.config.js
+++ webroot-dev/coolie.config.js
@@ -14,85 +14,85 @@
 'use strict';
 
 module.exports = function (coolie) {
-    // coolie 配置
-    coolie.config({
-        // 是否在构建之前清空目标目录
-        clean: true,
+  // coolie 配置
+  coolie.config({
+    // 是否在构建之前清空目标目录
+    clean: true,
 
-        // 目标配置
-        dest: {
-            // 目标目录，相对于当前文件
-            dirname: '../webroot-pro/',
-            // 目标根域
-            host: '/',
-            // 版本号长度
-            versionLength: 32
-        },
+    // 目标配置
+    dest: {
+      // 目标目录，相对于当前文件
+      dirname: '../webroot-pro/',
+      // 目标根域
+      host: '/',
+      // 版本号长度
+      versionLength: 32
+    },
 
-        // js 构建
-        js: {
-            // 入口模块，相对于当前文件
-            main: [
-                // 支持 glob 语法
-                //'./static/js/main/**/*.js'
-            ],
-            // coolie-config.js 路径，相对于当前文件
-           //'coolie-config.js': './static/js/coolie-config.js',
-		     'coolie-config.js':null,
-            // js 文件保存目录，相对于 dest.dirname
-            dest: './static/js/',
-            // 分块配置
-            chunk: [],
-            // js 压缩配置
-            minify: {
-                global_defs: {
-                    DEBUG: false
-                }
-            }
-        },
+    // js 构建
+    js: {
+      // 入口模块，相对于当前文件
+      main: [
+        // 支持 glob 语法
+        //'./static/js/main/**/*.js'
+      ],
+      // coolie-config.js 路径，相对于当前文件
+      //'coolie-config.js': './static/js/coolie-config.js',
+      'coolie-config.js': './coolie-config.js',
+      // js 文件保存目录，相对于 dest.dirname
+      dest: './static/js/',
+      // 分块配置
+      chunk: [],
+      // js 压缩配置
+      minify: {
+        global_defs: {
+          DEBUG: false
+        }
+      }
+    },
 
-        // html 构建
-        html: {
-            // html 文件，相对于当前文件
-            src: [
-                // 支持 glob 语法
-                //'./.views/**/*.html'
-				'*.html'
-            ],
-            // html 压缩配置
-            minify: true
-        },
+    // html 构建
+    html: {
+      // html 文件，相对于当前文件
+      src: [
+        // 支持 glob 语法
+        //'./.views/**/*.html'
+        '*.html'
+      ],
+      // html 压缩配置
+      minify: true
+    },
 
-        // css 构建
-        css: {
-            // css 文件保存目录，相对于 dest.dirname
-            dest: './static/css/',
-            // css 压缩配置
-            minify: true
-        },
+    // css 构建
+    css: {
+      // css 文件保存目录，相对于 dest.dirname
+      dest: './static/css/',
+      // css 压缩配置
+      minify: true
+    },
 
-        // 资源
-        resource: {
-            // 资源保存目录，相对于 dest.dirname
-            dest: './static/res/',
-            // 是否压缩
-            minify: true
-        },
+    // 资源
+    resource: {
+      // 资源保存目录，相对于 dest.dirname
+      dest: './static/res/',
+      // 是否压缩
+      minify: true
+    },
 
-        // 原样复制文件，相对于当前文件
-        copy: [
-            // 支持 glob 语法
-            //'./favicon.ico',
-            //'./robots.txt'
-        ]
-    });
+    // 原样复制文件，相对于当前文件
+    copy: [
+      // 支持 glob 语法
+      //'./favicon.ico',
+      //'./robots.txt'
+    ]
+  });
 
-    // 使用 coolie 中间件
-    // coolie.use(require('coolie-*'));
+  // 使用 coolie 中间件
+  // coolie.use(require('coolie-*'));
 
-    // 自定义 coolie 中间件
-    //coolie.use(function (options) {
-    //    // do sth.
-    //    return options;
-    //});
+  // 自定义 coolie 中间件
+  //coolie.use(function (options) {
+  //    // do sth.
+  //    return options;
+  //});
 };
